c589bcc22e7b5c655c869e63f5d36245a8cb0710
Bahtiev2Dinislam <dinislamka.bahti2018@gmail.com>
2026-09-20 21:19:52 +0300

    homework_09_14

---
+++ homework_2026_09_14/build_sort.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CPP_FILES="io.cpp sortings.cpp main.cpp"
+EXE="my_correct_sort"
+CHARSET="-finput-charset=utf-8 -fexec-charset=UTF-8"
+
+if [ -f "$EXE" ]; then
+    rm "$EXE"
+fi
+
+g++ $CHARSET $CPP_FILES -o $EXE
+
+./$EXE
